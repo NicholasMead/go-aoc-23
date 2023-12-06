a126d67591e1f2fd7e9ef62bba815782c1f1161b
Nick Mead <nick.mead.contractor@dvla.gov.uk>
2023-12-06 09:47:13 +0000

    nanoseconds!

--- y23d6/main.go
+++ y23d6/main.go
@@ -73,7 +73,7 @@ func main() {
 	})
 	fmt.Printf("Part 1: %v\n", p1)
 	fmt.Printf("Part 2: %v\n", p2)
-	fmt.Printf("Time: %vus\n", d.Microseconds())
+	fmt.Printf("Time: %vns\n", d.Nanoseconds())
 }
 
 func Simulate(input []Race) int {
